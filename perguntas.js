52c3a87ce3ea021bc04ca4a2af940521368fb45b
luan1231231 <168436537+luan1231231@users.noreply.github.com>
2024-12-02 09:22:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciências', 
+    'Qual é a fórmula química da água?', 
+    'A fórmula química da água é H2O'
+)
+
+criaCartao(
+    'Geografia', 
+    'Qual é o maior rio do Brasil', 
+    'O maior rio do Brasil é o Amazonas'
+)
+
+criaCartao(
+    'Língua Portuguesa', 
+    'O que é uma metáfora?', 
+    'Uma metáfora é uma figura de linguagem que compara duas coisas diferentes sem usar as palavras "como" ou "tal qual"'
+)
+
+criaCartao(
+    'Atualidade', 
+    'Qual é o nome do atual presidente dos Estados Unidos?', 
+    'O atual presidente dos Estados Unidos é Joe Biden'
+)
